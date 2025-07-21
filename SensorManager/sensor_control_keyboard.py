6652e74637ec11f82a62c777f3465d2407c44b19
sujatha <sh.sujathahariharan@gmail.com>
2025-07-21 13:42:32 +0200

    Sensor Manager

--- SensorManager/sensor_control_keyboard.py
+++ SensorManager/sensor_control_keyboard.py
@@ -1,3 +1,7 @@
+"""Function to control sensor_manager.py using keyboard inputs"""
+"""Enables synchronous activation and deactivation of all sensors used"""
+"""Requires sudo permission"""
+
 import threading
 import keyboard
 import time
@@ -5,7 +9,7 @@ import time
 
 def sensor_control_keyboard(start_event: threading.Event, stop_event: threading.Event):
     """
-    Pressing 'r' starts recording and 's' stops the program and recording
+    Pressing 'r' starts recording and pressing 's' stops the program and recording
     """
     print("Press 'r' to start recording and 's' to stop the program.")
     while not stop_event.is_set():
--- SensorManager/sensor_manager.py
+++ SensorManager/sensor_manager.py
@@ -1,5 +1,5 @@
 """Sensor manager for capturing data from multiple sensors and logging proprioception of a UR cobot"""
-"""Includes functions from proprioception_logger.py , camera_capture.py"""
+"""Includes functions from sensor_control_keyboard.py, proprioception_logger.py , camera_capture.py, digit_capture.py, get_unique_filename.py"""
 """Tested using a UR3e cobot"""
 """Run as sudo due to keyboard control of recording"""
 
@@ -53,14 +53,14 @@ digit_thread = threading.Thread(
     target=digit_capture,
     args=(root_dir, start_event, stop_event, digit_frame_queues),
 )
-digit_thread.start()
+digit_thread.start() # Start DIGIT thread
 
 # Thread for proprioception logging
 prop_thread = threading.Thread(
     target=proprioception_logger,
     args=(root_dir, start_event, stop_event, robot_ip),
 )
-prop_thread.start()
+prop_thread.start() # Start proprioception logging thread
 
 # Live preview loop
 print("Waiting for sensors to start...")
@@ -72,9 +72,9 @@ while not stop_event.is_set():
             cv2.imshow(f"Camera {cam_id}", frame)
 
     # Show DIGIT frames
-    for serial, dq in digit_frame_queues.items():
-        if not dq.empty():
-            serial, frame = dq.get()
+    for serial, digit_queue in digit_frame_queues.items():
+        if not digit_queue.empty(): # Gets frame if one is available in queue
+            serial, frame = digit_queue.get()
             if frame is not None:
                 cv2.imshow(f"Digit {serial}", frame)
 
@@ -87,6 +87,7 @@ while not stop_event.is_set():
 camera_thread.join()
 digit_thread.join()
 prop_thread.join()
+
 # Close live preview
 cv2.destroyAllWindows()
 print("All sensors stopped.")
